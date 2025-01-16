9887a02cf4d91dcef36f7d657783a06b42cbb206
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-17 02:28:43 +0800

    - add build-in-docker.sh

---
+++ build-in-docker.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+function announce () {
+    echo "##########################################################################################"
+    echo "##############################  $*  #################################"
+    echo "##########################################################################################"
+}
+
+git clone https://github.com/abcminiuser/lufa.git && \
+  cd lufa && \
+  ls
